ecdd4d51ed6effdda236b9850fc11e0f612a01f1
adlzanchetta <adlzanchetta@gmail.com>
2023-01-04 22:10:01 -0500

    Updates

--- component---src-pages-portfolio-js-de1ea7517fd73c3519a8.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[25],{4037:function(e,t,l){l.r(t),l.d(t,{default:function(){return u}});var n=l(7294),r=l(7782),a=l(4553);function u(){var e=(0,r.useTranslation)().t;return n.createElement(a.Z,null,n.createElement("p",null,n.createElement("h1",null,e("portfolio"))),n.createElement("p",null,"TODO"))}}}]);
-//# sourceMappingURL=component---src-pages-portfolio-js-de1ea7517fd73c3519a8.js.map
