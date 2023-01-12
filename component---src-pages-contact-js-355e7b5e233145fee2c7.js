cbaddb2305375ae6226bf55cdc028a30f13b6398
adlzanchetta <adlzanchetta@gmail.com>
2023-01-12 18:48:48 -0500

    Updates

--- component---src-pages-contact-js-355e7b5e233145fee2c7.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[501],{5791:function(e,t,l){l.r(t),l.d(t,{default:function(){return u}});var n=l(7294),r=l(7782),a=l(241);function u(){const{t:e}=(0,r.useTranslation)();return n.createElement(a.Z,null,n.createElement("p",null,n.createElement("h1",null,e("contact"))),n.createElement("p",null,"TODO"))}}}]);
-//# sourceMappingURL=component---src-pages-contact-js-355e7b5e233145fee2c7.js.map
